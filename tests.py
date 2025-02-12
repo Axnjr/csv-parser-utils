35477ff7c2c97156d3e803eb3607e0fc268ab723
Yakshit Chhipa <yakshitchhipa@gmail.com>
2025-02-12 06:16:29 +0530

    fix: column name in remove_duplicates() from "Job" to "Job Tittle"

--- tests.py
+++ tests.py
@@ -62,7 +62,7 @@ def run_test4():
     df.replace_all_vals("First Name", "Shelby", "Radha")
 
     # removes duplicates from "Job" Column
-    df.remove_duplicates("Job")
+    df.remove_duplicates("Job Title")
 
     # Converts all first names to uppercase
     df.apply_func("First Name", lambda s: s.upper())  
